2b5016829b700fd4313bb8835fc2d9540f61154a
Jethro <jethrotiago@hotmail.com>
2023-10-09 22:54:57 -0300

    added weather images

---
+++ assets/javascript/index.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container');
+const pesquisa = document.querySelector('.caixa-pesquisa button');
+const caixaClima = document.querySelector('.caixa-clima');
+const detalhesClima = document.querySelector('.detalhes-clima');
+const erro404 = document.querySelector('.nao-encontrado');
